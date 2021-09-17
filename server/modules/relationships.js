4122839e0137561de3bdce195066452a873b847f
Jiacheng Li <guslijif@student.gu.se>
2021-09-17 14:33:27 +0800

    fix bugs in relationships

--- server/modules/relationships.js
+++ server/modules/relationships.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 var relationship = new Schema({
-    e_bank_id : Schema.Types.ObjectId,
-    user_id : Schema.Types.ObjectId,
+    e_bank_id : String,
+    user_id : String,
 });
 
 module.exports = mongoose.model('relationship', relationship);
