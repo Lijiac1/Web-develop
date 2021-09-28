92a4632fbee5af4ae75a8c2c0b7ce183703366ec
Jiacheng Li <guslijif@student.gu.se>
2021-09-28 23:55:18 +0800

    bassic front_end and weird relationship controller

--- client/src/components/navagator.vue
+++ client/src/components/navagator.vue
@@ -1,8 +1,12 @@
 <template>
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/Home">Home</router-link>
       <router-link to = "/E_bank">E_bank</router-link>
       <router-link to = "/Games">Games</router-link>
 
     </div>
 </template>
+
+<style scoped>
+
+</style>
