1aefdf0f2fa0f2ca51063c3b9c0d71d3a9d40bd9
Jiacheng Li <guslijif@student.gu.se>
2021-09-30 16:18:16 +0800

    change the navbar

---
+++ client/src/components/E_bank.vue
@@ -0,0 +1,19 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col>
+                <b-card
+                border-variant="primary"
+                header="E-bank info"
+                header-bg-variant="primary"
+                header-text-variant="white"
+
+                >
+                <label for="textarea-small">Total money:</label>
+                <label for="textarea-small">Total chips:</label>
+                </b-card>
+            </b-col>
+
+        </b-row>
+    </b-container>
+</template>
