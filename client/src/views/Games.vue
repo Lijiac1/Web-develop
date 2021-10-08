e13925d0a22fa6fa93da7831703208c4c0a3b6ea
yuhanl <yuhanl@student.chalmers.se>
2021-10-09 02:08:10 +0800

    update the game page

--- client/src/views/Games.vue
+++ client/src/views/Games.vue
@@ -1,27 +1,34 @@
 <template>
     <div>
-      <b-jumbotron >
-        <b-container >
-
-         <navagator/>
+      <navagator/>
+        <b-container fluied= "md">
         <h1>Games</h1>
+<b-row class="mt-5">
+          <b-col cols="8">Computer:</b-col>
+          <b-col cols="1">{{}}</b-col>
+</b-row>
+<b-row class="mt-5">
+      <b-col cols="8">You:</b-col>
+    <b-col cols="1"> {{}} </b-col>
+</b-row>
 
-    <template v-slot:header>BootstrapVue</template>
+  <b-row class="mt-5">
+      <b-col cols="8">Resoults:</b-col>
+      <b-col cols="1">{{}}</b-col>
+  </b-row>
 
-    <template v-slot:lead></template>
-    <div>
-  <b-img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20170828%2Fab4eaea5e401470abb0a0458354221da.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1635585744&t=cab9f967fe2e35f03de379e27720e4fd" fluid alt="Responsive image"></b-img>
-    </div>
-
-  <b-row>
-    <b-col><b-button variant="primary" href="#">Another card</b-button></b-col>
-    <b-col><b-button variant="success" href="#">Stop taking cards</b-button></b-col>
+  <b-row class="mt-5">
+    <b-col><b-button variant="primary" href="#">Take a card</b-button></b-col>
     <b-col><b-button variant="danger" href="#">Leave the game</b-button></b-col>
+  </b-row>
+  <b-row class="mt-5">
+    <b-col><b-text>place the Chips</b-text></b-col>
+    <b-col ><b-button variant="primary">50</b-button></b-col>
+    <b-col col lg="1"><b-button variant="primary">100</b-button></b-col>
+    <b-col><b-button variant="primary">All in!</b-button></b-col>
   </b-row>
 
     </b-container >
-
-  </b-jumbotron>
 </div>
 </template>
 
