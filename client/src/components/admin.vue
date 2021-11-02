1a9ac0b2621142df436ed878babbe7c5f7920eeb
Jiacheng Li <guslijif@student.gu.se>
2021-11-02 14:35:16 +0800

    update the methods in admin page

--- client/src/components/admin.vue
+++ client/src/components/admin.vue
@@ -2,17 +2,40 @@
     <b-container class="mt-3">.
         <h3>E_bank</h3>
         <b-row>
-              <b-col>
+            <b-col>
+              <b-card class="mt-5" no-body>
+               <b-nav pills card-header slot="header" >
+                 <b-col><b-nav-item>Total Money</b-nav-item></b-col>
+                 <b-col><b-nav-item>Total Chips</b-nav-item></b-col>
+                 <b-col><b-nav-item>Money In</b-nav-item></b-col>
+                 <b-col><b-nav-item>Money Out</b-nav-item></b-col>
+                 <b-col><b-nav-item>Chips In </b-nav-item></b-col>
+                 <b-col><b-nav-item>Chips Out</b-nav-item></b-col>
+                 <b-col></b-col>
+                 <b-col></b-col>
+               </b-nav>
                <b-card
                 border-variant="primary"
                 header-bg-variant="primary"
                 header-text-variant="white"
-                class="mt-5"
-                style="min-height: 234px; overflow-y:scroll;"
+                style="position:relative; height: 234px; overflow-y:scroll;"
                 >
+                <b-row v-for="eBank in eBanks" :key="eBank.id">
+                  <b-col>{{eBank.total_money}}</b-col>
+                  <b-col>{{eBank.total_chips}}</b-col>
+                  <b-col>{{eBank.money_in}}</b-col>
+                  <b-col>{{eBank.money_out}}</b-col>
+                  <b-col>{{eBank.chips_in}}</b-col>
+                  <b-col>{{eBank.chips_out}}</b-col>
+                  <b-col><b-button variant="outline-primary" @click="select(eBank._id)">Select</b-button></b-col>
+                  <b-col><b-button variant="outline-primary" @click="checkUsers(eBank._id)">Users</b-button></b-col>
+                </b-row>
                 </b-card>
-            </b-col></b-row>
-        <b-row class="ebank_info">
+              </b-card>
+            </b-col>
+
+            </b-row>
+            <b-row class="ebank_info">
             <b-col>
                 <b-card
                 border-variant="primary"
@@ -116,7 +139,7 @@ export default {
       MoneyOut: 0,
       ChipsIn: 0,
       ChipsOut: 0,
-      eBanks_id: null,
+      eBank_id: null,
       users: [],
       eBanks: []
 
@@ -129,47 +152,79 @@ export default {
   },
   methods: {
     modifyMoney(event) {
-      Api.patch(`/e_banks/${this.eBanks_id}`, { total_money: this.ModifyMoney }).then(response => {
+      Api.patch(`/e_banks/${this.eBank_id}`, { total_money: this.ModifyMoney }).then(response => {
         console.log(response.status)
-      }).catch(error => {
+        this.TotalMoney = this.ModifyMoney
+        this.ModifyMoney = 0
+        return Api.get('/e_banks')
+      }).then(response => { this.eBanks = response.data.e_banks }
+      ).catch(error => {
         console.log(error)
       })
     },
     modifyChips(event) {
-      Api.patch(`/e_banks/${this.eBanks_id}`, { total_chips: this.ModifyChips }).then(response => {
+      Api.patch(`/e_banks/${this.eBank_id}`, { total_chips: this.ModifyChips }).then(response => {
         console.log(response.status)
-      }).catch(error => {
+        this.TotalChips = this.ModifyChips
+        this.ModifyChips = 0
+        return Api.get('/e_banks')
+      }).then(response => { this.eBanks = response.data.e_banks }
+      ).catch(error => {
         console.log(error)
+      })
+      Api.get('/e_banks').then(response => {
+        this.eBanks = response.data.e_banks
       })
     },
     createEbank(event) {
       const b = {
         total_money: this.TotalMoneyInput,
-        total_chips: this.TotalChips,
+        total_chips: this.TotalChipsInput,
         money_in: 0,
         money_out: 0,
         chips_in: 0,
         chips_out: 0
       }
-      Api.post(`/e_banks/${this.eBanks_id}`, b).then(response => {
+      Api.post('/e_banks', b).then(response => {
         if (response.status === 201) {
           alert('create successfully')
         } else {
           alert('fail to creat ebank')
         }
+        return Api.get('/e_banks')
+      }).then(response => {
+        this.eBanks = response.data.e_banks
       }).then(error => {
         console.log(error)
       })
     },
     deleteEbank(event) {
-      Api.delete('/e_banks').then(response => {
+      Api.delete(`/e_banks/${this.eBank_id}`).then(response => {
         if (response.status === 204) {
           alert('Delete successfully')
         } else {
           alert('fail to delete')
         }
-      }).then(error => {
+        return Api.get('/e_banks')
+      }).then(response => {
+        this.eBanks = response.data.e_banks
+      }).catch(error => {
         console.log(error)
+      })
+    },
+    select(id) {
+      this.eBank_id = id
+      Api.get(`/e_banks/${this.eBank_id}`).then(response => {
+        this.TotalMoney = response.data.total_money
+        this.TotalChips = response.data.total_chips
+        this.TotalMoneyInput = response.data.money_in
+        this.TotalChipsInput = response.data.money_out
+      })
+    },
+    checkUsers(ebankid) {
+      Api.get(`/e_banks/${ebankid}/users/name`).then(response => {
+        this.users = JSON.stringify(response.data)
+        alert(this.users)
       })
     }
   }
